780e7007535af2b912abe1a466a39622673d9e11
ekezieprosper <japhethprosper234@gmail.com>
2024-09-16 21:45:01 +0100

    elitefootball

--- models/otpModel.js
+++ models/otpModel.js
@@ -11,7 +11,7 @@ const otpSchema = new mongoose.Schema({
     createdAt: {
         type: Date, 
         default: Date.now,
-        expires: '5m'
+        expires: '10m'
     },
 
     playerId: [{
